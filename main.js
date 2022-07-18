5e30f6542346b460dfd5401c442be23beed74759
MohamedShalaSh <102245899+MohamedShalaSh@users.noreply.github.com>
2022-07-18 16:20:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ const THEfun2=()=>{
     priceINSecondPage3.innerHTML=`${in3.value*num3} $`
     //total
     let totalPrice=document.getElementById('totalPrice');
-    totalPrice.innerHTML= `Total | <p  class="totalEnd"${(in1.value*num1)+(in2.value*num2)+(in3.value*num3)}</p> $`
+    totalPrice.innerHTML= `Total | ${(in1.value*num1)+(in2.value*num2)+(in3.value*num3)}$`
 }
 
 
